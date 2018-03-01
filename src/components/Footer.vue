df39304d7d8dd9b9b421bb6c203afddcaa099d72
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-03-01 10:49:35 -0500

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Intelligent Cloud Azure Container Hackfest
+        Intelligent Cloud Azure Container Hackfest Pittsburgh
       </div>
       <div class="col-lg-6">
       </div>
