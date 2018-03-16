190a04ece3624925648c99f1936ba4e765a3e4f0
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-03-16 12:09:48 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Kubernetes Meetup
+        Kubernetes Testing
       </div>
       <div class="col-lg-6">
       </div>
