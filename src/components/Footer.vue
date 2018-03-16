49661736048a75ddda7a2c3d368c2bd363ddadb3
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-03-16 16:12:12 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Kubernetes Hackfest Chile
+        Kubernetes Hackfest
       </div>
       <div class="col-lg-6">
       </div>
