20c8942abca2bac2d5dfa8efb183b61318ced5c6
Joey Schluchter <jschluchter@gmail.com>
2018-03-20 14:46:45 -0400

    Update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Kubernetes Hackfest Colombia
+        Kubernetes Hackfest Colombia && Atlanta
       </div>
       <div class="col-lg-6">
       </div>
