59366f9884edb1a7309a5e9a9580a032f40f722c
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-04-12 14:04:06 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Kubernetes Service Rocks!
+        Pittsburgh Weather is terrible
       </div>
       <div class="col-lg-6">
       </div>
