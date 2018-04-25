038efc1bd06f30f371437056a3bd5bb588e3ae73
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-04-25 19:42:06 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Burlington is not Boston
+        Brian is super cool
       </div>
       <div class="col-lg-6">
       </div>
