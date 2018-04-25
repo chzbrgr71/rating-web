07294f519929757f5c151127b39c96c81ea73576
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-04-25 14:20:16 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        MOVING TO CALIFORNIA
+        Burlington is not Boston
       </div>
       <div class="col-lg-6">
       </div>
