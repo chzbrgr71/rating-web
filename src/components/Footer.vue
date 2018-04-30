f61b0168f79e2cefbf03fab0393b64f1c89a3e23
Brian Redmond <brianisrunning@gmail.com>
2018-04-30 15:15:20 +0200

    changed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Container Day - Copenhagen
+        Azure Container Day - Lyngby
       </div>
       <div class="col-lg-6">
       </div>
